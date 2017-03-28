16f7196d3db28455486291bd266d99fb07c81a6b
Christopher Stewart <christopherprattstewart@gmail.com>
2017-03-28 11:46:41 -0400

    Added test file

---
+++ test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 11:19:12 2017
+
+@author: christopherstewart
+"""
+import numpy as np
+
+class Neural_Network(object):
+    
+    def __init__(self):
+        #Parameters
+        self.inputLayerSize = 2
+        self.outputLayerSize =1
+        self.hiddenLayerSize =3
+        #Weights 
+        self.W1 = np.random.randn(self.inputLayerSize,
+                                  self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize,
+                                  self.outputLayerSize)
+        
+    def forward(self, X):
+        #move a step forward
+        self.z2 = np.dot(X,self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2,self.W2)
+        yHat = self.sigmoid(self.z3)
+        return yHat
+        
+        
+    def sigmoid(self, z):
+        return 1/(1+np.exp(-z))
+        
